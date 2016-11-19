bb496fc7126eadf085e5e159a1dab410a5b97042
teemhunduru <tm347@student.anglia.ac.uk>
2016-11-19 00:40:06 +0000

    Updated Select form

--- temp1/temp1/SelectEditDelete.cs
+++ temp1/temp1/SelectEditDelete.cs
@@ -11,7 +11,7 @@ namespace temp1
         {
             InitializeComponent();
         }
-        public static SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Najat\Desktop\SEAssignment\GroupAssignment7\temp1\temp1\HappyTechDatabase.mdf;Integrated Security=True;Connect Timeout=30");
+       
         private void lblTemplateName_Click(object sender, EventArgs e)
         {
 
@@ -19,84 +19,73 @@ namespace temp1
 
         private void cmbTempID_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //opening the connection to the database
-            con.Open();
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "select * from Template where TemplateID = '" + cmbTempID.SelectedItem.ToString() + "'";
-            cmd.ExecuteNonQuery();
-            DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(dt);
-            //filling the text boxes
-            foreach (DataRow dr in dt.Rows)
+            //fill in the grid 
+            DataSet dsPerson = DatabaseConnection.getDBConnectionInstance().getDataSet("select * from Template where TemplateID = '" + cmbTempID.SelectedItem.ToString() + "'");
+
+
+            //get the table from the dataset
+            DataTable dtPerson = dsPerson.Tables[0];
+
+            //set up the data grid view
+            foreach (DataRow dr in dsPerson.Tables[0].Rows)
             {
                 txtTemplateName.Text = dr["TemplateName"].ToString();
                 txtPosition.Text = dr["Position"].ToString();
                 txtHeading.Text = dr["Heading"].ToString();
                 txtSubHeading.Text = dr["SubHeading"].ToString();
                 txtComment.Text = dr["Comment"].ToString();
-                // txtSubHeading2.Text = dr[""].ToString();
-                // txtComment2.Text = dr["Contact"].ToString();
+                
             }
-            //closing the connection to the database
-            con.Close();
+           
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            //To update template column
-            SqlCommand command = new SqlCommand();
-            command.Connection = con;
-            command.CommandType = CommandType.Text;
-            //opening the connection to the database
-            con.Open();
-            //updating data to database
-            command.CommandText = "update Template set TemplateName='" + txtTemplateName.Text + "', Heading= '" + txtHeading.Text + "', SubHeading= '" + txtSubHeading.Text + "', Comment= '" + txtComment.Text + "', Position= '" + txtPosition.Text + "' where TemplateID= '" + cmbTempID.SelectedItem.ToString() +"'";
-            command.ExecuteNonQuery();
-            //closing the connection to the database
-            con.Close();
+            //fill in the grid 
+            DataSet dsPerson = DatabaseConnection.getDBConnectionInstance().getDataSet("update Template set TemplateName='" + txtTemplateName.Text + "', Heading= '" + txtHeading.Text + "', SubHeading= '" + txtSubHeading.Text + "', Comment= '" + txtComment.Text + "', Position= '" + txtPosition.Text + "' where TemplateID= '" + cmbTempID.SelectedItem.ToString() + "'");
+
             //confirming data is updated
             MessageBox.Show("Datas Updated");
+
+            txtTemplateName.Clear();
+            txtPosition.Clear();
+            txtHeading.Clear();
+            txtSubHeading.Clear();
+            txtComment.Clear();
+            
+
         }
 
         private void SelectEditDelete_Load(object sender, EventArgs e)
         {
 
-            //opening the connection to the database
-            con.Open();
-            //adding all staff ID 
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "select * from Template";
-            cmd.ExecuteNonQuery();
-            DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(dt);
-            // fillin template information into the text boxes
-            foreach (DataRow dr in dt.Rows)
+            //fill in the grid 
+            DataSet dsPerson = DatabaseConnection.getDBConnectionInstance().getDataSet("select * from Template");
+
+
+            //get the table from the dataset
+            DataTable dtPerson = dsPerson.Tables[0];
+
+            foreach (DataRow dr in dsPerson.Tables[0].Rows)
             {
                 cmbTempID.Items.Add(dr["TemplateID"].ToString());
             }
-            //closing the connection to the database
-            con.Close();
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            //To delete template column
-            SqlCommand command = new SqlCommand();
-            command.Connection = con;
-            command.CommandType = CommandType.Text;
-            //opening the connection to the database
-            con.Open();
-            //deleting data to database
-            command.CommandText = "delete from Template where TemplateID = '" + cmbTempID.SelectedItem.ToString() + "'";
-            command.ExecuteNonQuery();
-            //closing the connection to the database
-            con.Close();
+            //fill in the grid 
+            DataSet dsPerson = DatabaseConnection.getDBConnectionInstance().getDataSet("delete from Template where TemplateID = '" + cmbTempID.SelectedItem.ToString() + "'");
+
             //confirming data is deleted
             MessageBox.Show("Datas Deleted");
+
+            txtTemplateName.Clear();
+            txtPosition.Clear();
+            txtHeading.Clear();
+            txtSubHeading.Clear();
+            txtComment.Clear();
+
         }
 
         private void btnExit_Click(object sender, EventArgs e)
